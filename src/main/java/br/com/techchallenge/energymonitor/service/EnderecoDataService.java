0d75db4ba7cb6147f21d39ead1623c27de9b1bbe
kesaraujo <kesaraujo@gmail.com>
2023-08-06 18:27:14 -0300

    Incluir @Primary em EnderecoDataService

--- src/main/java/br/com/techchallenge/energymonitor/service/EnderecoDataService.java
+++ src/main/java/br/com/techchallenge/energymonitor/service/EnderecoDataService.java
@@ -2,9 +2,11 @@ package br.com.techchallenge.energymonitor.service;
 
 import br.com.techchallenge.energymonitor.dominio.Endereco;
 import br.com.techchallenge.energymonitor.repository.EnderecoRepository;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class EnderecoDataService extends DataService<Endereco> {
 
     public EnderecoDataService(EnderecoRepository repository) {
