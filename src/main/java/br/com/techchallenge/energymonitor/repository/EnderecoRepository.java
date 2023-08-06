26a2bef69f03a8cd1c6ec3cac56675e25bc3f245
kesaraujo <kesaraujo@gmail.com>
2023-08-06 19:04:27 -0300

    Aturalizar user/password

--- src/main/java/br/com/techchallenge/energymonitor/repository/EnderecoRepository.java
+++ src/main/java/br/com/techchallenge/energymonitor/repository/EnderecoRepository.java
@@ -3,4 +3,16 @@ package br.com.techchallenge.energymonitor.repository;
 import br.com.techchallenge.energymonitor.dominio.Endereco;
 
 public interface EnderecoRepository extends BaseRepository<Endereco> {
+
+//    Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+//
+//    Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
+//
+//    @Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+//    Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+//
+//
+//    @Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+//    Page<User> searchName(String name, Pageable pageable);
+
 }
